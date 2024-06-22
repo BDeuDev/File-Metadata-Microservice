6684e1c3448f60fe4b67f37c160a1f4c41529f01
BDeuDev <bdeusich.dev@gmail.com>
2024-06-22 17:01:07 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const upload_routes = require('./routes/upload_routes');
+const dotenv = require('dotenv');
+dotenv.config()
+
+
+app.use(cors({optionsSuccessStatus: 200})); 
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/api',upload_routes);
+
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
